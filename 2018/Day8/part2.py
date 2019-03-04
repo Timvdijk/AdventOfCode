5e20bb64a6f4c45d75af28e4df5346b8998e5060
Tim <tim.vandijk@student.hu.nl>
2019-03-04 20:26:56 +0100

    completed day8 p2

--- 2018/Day8/part2.py
+++ 2018/Day8/part2.py
@@ -1,5 +1,28 @@
 with open("input.txt") as file:
-    line = [int(i) for i in file.readline().strip().split(' ')]
+    line = iter(map(int, file.readline().strip().split(' ')))
 
-queue = [[line[0], line[1]]]
-position = 0
+def read_tree():
+    next_child, next_metadata = next(line), next(line)
+    children, metadata = [], []
+
+    for i in range(next_child):
+        children.append(read_tree())
+    for i in range(next_metadata):
+        metadata.append(next(line))
+
+    return (children, metadata)
+
+def value(node):
+    if not node[0]:
+        return sum(node[1])
+    else:
+        count = 0
+
+        for m in node[1]:
+            if 1 <= m <= len(node[0]):
+                count += value(node[0][m - 1])
+
+        return count
+
+
+print(value(read_tree()))
