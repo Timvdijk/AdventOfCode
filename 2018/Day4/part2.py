9c6037fd80d9be3770e547c19859fb4854143623
Tim <tim.vandijk@student.hu.nl>
2019-03-04 21:53:29 +0100

    minified

--- 2018/Day4/part2.py
+++ 2018/Day4/part2.py
@@ -12,18 +12,14 @@ for line in chronological:
     elif line[2] == "falls":
         start_t = line[1][3:5] if line[1][:2] != "23" else "00"
     elif line[2] == "wakes":
-        try:
-            timetable[guard_id][0] += int(line[1][3:5]) - int(start_t)
-
-            for i in range(int(start_t), int(line[1][3:5])):
-                timetable[guard_id][int(str(i)[0]) + 1 if i > 10 else 1][(int(str(i)[1]) % 10) if i > 9 else int(str(i)[0])] += 1
-        except KeyError:
+        if guard_id not in timetable:
             timetable[guard_id] = [[0 for i in range(10)] for i in range(6)]
             timetable[guard_id].insert(0, int(line[1][3:5]) - int(start_t))
+        else:
+            timetable[guard_id][0] += int(line[1][3:5]) - int(start_t)
 
-            for i in range(int(start_t), int(line[1][3:5])):
-                timetable[guard_id][int(str(i)[0]) + 1 if i > 10 else 1][(int(str(i)[1]) % 10) if i > 9 else int(str(i)[0])] += 1
-
+        for i in range(int(start_t), int(line[1][3:5])):
+            timetable[guard_id][int(str(i)[0]) + 1 if i > 10 else 1][(int(str(i)[1]) % 10) if i > 9 else int(str(i)[0])] += 1
 
 max_pair = max(([max([max(ten) for ten in timetable[k][1:]]), k] for k in timetable), key=lambda x: x[0])
 
