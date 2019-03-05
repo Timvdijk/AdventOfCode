b18dfeef88af4dddaacae40b7cc38986e40427b3
Tim <tim.vandijk@student.hu.nl>
2019-03-05 19:34:35 +0100

    day 9 should be working

--- 2018/Day9/part2.py
+++ 2018/Day9/part2.py
@@ -1,9 +1,10 @@
+from collections import deque
 with open("input.txt") as file:
     line = file.read().strip().split(' ')
 
-max_players, max_points = int(line[0]), int(line[-2])
+max_players, max_points = int(line[0]), int(line[-2] * 100)
 
-marbles = [0]
+marbles = deque([0])
 current = 0
 player = 0
 scores = dict()
@@ -17,11 +18,11 @@ for marble in range(1, max_points + 1):
             scores[player] = 0
 
         scores[player] += marble
-        current = (current - 7 + len(marbles)) % len(marbles)
-        scores[player] += marbles.pop(current)
+        marbles.rotate(-7)
+        scores[player] += marbles.pop()
     else:
-        current = (current + 2) % len(marbles)
-        marbles.insert(current, marble)
+        marbles.rotate(2)
+        marbles.append(marble)
 
     player += 1
 
